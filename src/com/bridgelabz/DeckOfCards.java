81f3e01dd62f90bf3438e74215d812e2462d54f5
Sakshi <sakshishetty1231997@gmail.com>
2021-08-18 16:31:39 +0530

    Added: Welcome message to the program

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement object oriented programs and JSON
+ * @author - Sakshi Shetty
+ * @version - 8.0
+ * @since - 2021-08-18
+ */
+public class DeckOfCards {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Program");
+	}
+}
+	
